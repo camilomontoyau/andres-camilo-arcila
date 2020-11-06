04a1466c7ee39c77b358588bbfd89d8c0b74f500
camilomontoyau <camilomontoya2@gmail.com>
2020-11-06 15:27:04 -0500

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Component, useState, useEffect } from "react";
+
+function App() {
+  const [personajes, setPersonajes] = useState([]);
+  useEffect(() => {
+    // vas a usar axios
+    // vas a usar async / await
+    fetch("https://swapi.dev/api/people/")
+      .then((r) => r.json())
+      .then((response) => setPersonajes(response.results));
+  });
+  
+  return (
+    <div className="App">
+      <div>
+        <ol>
+          {personajes.map((personaje) => (
+            <ul key={personaje.name}>{personaje.name}</ul>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
