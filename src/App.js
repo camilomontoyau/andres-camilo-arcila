04e80115950073a64c1343d14a4e1c13f9a1154e
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-11-07 13:03:08 -0500

    Se agregó el uso de axios y asyn/await para fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
-import { Component, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
   const [personajes, setPersonajes] = useState([]);
   useEffect(() => {
-    // vas a usar axios
-    // vas a usar async / await
-    fetch("https://swapi.dev/api/people/")
-      .then((r) => r.json())
-      .then((response) => setPersonajes(response.results));
-  });
-  
+    async function fetchData() {
+      // fetch("https://swapi.dev/api/people/")
+      //   .then((r) => r.json())
+      //   .then((response) => setPersonajes(response.results));
+
+      // vas a usar axios
+      // axios.get("https://swapi.dev/api/people/")
+      //   .then((response) => setPersonajes(response.data.results));
+
+      // vas a usar async / await
+      const response = await fetch("https://swapi.dev/api/people/");
+      const json = await response.json();
+      setPersonajes(json.results);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <div>
