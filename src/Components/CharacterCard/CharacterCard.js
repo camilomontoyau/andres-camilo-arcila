a90c2296b2b5e6ebbc27d4625c60f1ce74927b6d
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-11-08 20:47:46 -0500

    Se creó el componente de card del personaje

---
+++ src/Components/CharacterCard/CharacterCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Styles from "./CharacterCard.module.sass";
+
+function CharacterCard({ data: { name, gender, birthYear, height, mass, eyeColor, hairColor, skinColor } }) {
+  return (
+    <div className={Styles["card--character"]}>
+      <div className={`${Styles["information"]} ${Styles["information--main"]}`}>
+        <h3 className={Styles["name"]}>{name.toLowerCase()}</h3>
+        <p className={Styles["gender"]}>{gender}</p>
+      </div>
+      <div className={`${Styles["information"]} ${Styles["information--secondary"]}`}>
+        <div className={Styles["data--container"]}>
+          <p className={Styles["label"]}>Birth year: </p><p className={Styles["data"]}>{birthYear}</p>
+        </div>
+        <div className={Styles["data--container"]}>
+          <p className={Styles["label"]}>Height: </p><p className={Styles["data"]}>{height} cm</p>
+        </div>
+        <div className={Styles["data--container"]}>
+          <p className={Styles["label"]}>Mass: </p><p className={Styles["data"]}>{mass} Kg</p>
+        </div>
+        <div className={Styles["data--container"]}>
+          <p className={Styles["label"]}>Eye color: </p><p className={Styles["data"]}>{eyeColor}</p>
+        </div>
+        <div className={Styles["data--container"]}>
+          <p className={Styles["label"]}>Hair color: </p><p className={Styles["data"]}>{hairColor}</p>
+        </div>
+        <div className={Styles["data--container"]}>
+          <p className={Styles["label"]}>Skin color: </p><p className={Styles["data"]}>{skinColor}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CharacterCard;
