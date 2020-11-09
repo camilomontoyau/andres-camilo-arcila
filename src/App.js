0d0b7a29ca5e32e78d7f83ca1590d1cdc79de9e8
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-11-08 20:48:37 -0500

    Se agregó el componente CharacterCard a la app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import CharacterCard from './Components/CharacterCard/CharacterCard';
+
+const translateKeys = (array) => {
+  return array.map(value => {
+    return ({
+      name: value.name,
+      birthYear: value.birth_year,
+      eyeColor: value.eye_color,
+      gender: value.gender,
+      hairColor: value.hair_color,
+      height: value.height,
+      mass: value.mass,
+      skinColor: value.skin_color
+    });
+  })
+}
 
 function App() {
-  const [personajes, setPersonajes] = useState([]);
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     async function fetchData() {
       // fetch("https://swapi.dev/api/people/")
       //   .then((r) => r.json())
-      //   .then((response) => setPersonajes(response.results));
+      //   .then((response) => setCharacters(translateKeys(response.results)));
 
       // vas a usar axios
-      // axios.get("https://swapi.dev/api/people/")
-      //   .then((response) => setPersonajes(response.data.results));
+      axios.get("https://swapi.dev/api/people/")
+        .then((response) => setCharacters(translateKeys(response.data.results)));
 
       // vas a usar async / await
-      const response = await fetch("https://swapi.dev/api/people/");
-      const json = await response.json();
-      setPersonajes(json.results);
+      // const response = await fetch("https://swapi.dev/api/people/");
+      // const json = await response.json();
+      // const charactersFetch = json.results;
+      // setCharacters(translateKeys(charactersFetch));
     }
     fetchData();
   }, []);
@@ -25,11 +42,16 @@ function App() {
   return (
     <div className="App">
       <div>
-        <ol>
+        <ol className="container--characters">
           {/* componente reusable para cada personaje */}
-          {personajes.map((personaje) => (
-            <ul key={personaje.name}>{personaje.name}</ul>
-          ))}
+          {characters.map((character) => {
+            return (
+              <div className="card--character--wrapper" key={character.name}>
+                <CharacterCard data={character} />
+              </div>
+            );
+          }
+          )}
         </ol>
       </div>
     </div>
