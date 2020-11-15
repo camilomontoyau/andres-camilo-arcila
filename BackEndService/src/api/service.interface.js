3b29fea6331a59bd6114e27837a735b7bbb2a3af
Andrés Camilo Arcila <acarcila98@gmail.com>
2020-11-15 00:52:26 -0500

    Se creó el interface para los componentes de servicio

---
+++ BackEndService/src/api/service.interface.js
@@ -0,0 +1,13 @@
+// import {Router} from 'express'
+
+export default class IService{
+    constructor(){
+        if(this.constructor == IService){
+            throw new Error("Abstract classes can't be instantiated.");
+        }
+    }
+
+    getRouter() {
+        throw new Error("Method must be implemented.")
+    }
+}
